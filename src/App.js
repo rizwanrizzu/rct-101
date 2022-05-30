f141f222dff14d2e47327586ba56efd5abfc6bbc
rizwanrizzu <rizwansyednasroddin@gmail.com>
2022-05-30 17:15:48 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import products from "../src/components/Products"
+
+const App = () => {
+  return (
+  <div>
+   <products/>
+  </div>
+  )
+};
+
+export default App;
---
+++ src/components/Product.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Pagination from "../components/Pagination";
+import Products from "../components/Products";
+import Addproduct from "../components/AddProduct";
+import {Flex,Grid} from "..@chakra-ui/react"
+
+const Product = () => {
+  // TODO: Remove below const and instead import them from chakra
+  // const Text = () => <div />;
+  // const Image = () => <div />;
+  // const Box = () => <div />;
+  // const Stack = () => <div />;
+  // const Heading = () => <div />;
+  // const Tag = () => <div />;
+  // const TagLabel = () => <div />;
+  return (
+    <Stack data-cy="product">
+      <Image data-cy="product-image" />
+      <Text data-cy="product-category"></Text>
+      <Tag>
+        <TagLabel data-cy="product-gender"></TagLabel>
+      </Tag>
+      <Heading data-cy="product-title"></Heading>
+      <Box data-cy="product-price"></Box>
+    </Stack>
+  );
+};
+
+export default Product;
